332bed4aa0f5dba7369edeb6fda6f3e870a79159
Sam Willingham <68390031+SamJamWillingham@users.noreply.github.com>
2020-09-15 16:48:05 -0400

    added target _blank to links

--- src/Credit.js
+++ src/Credit.js
@@ -5,22 +5,40 @@ export default function Credit() {
   return (
     <div id="credit">
       <small>
-        <a href="https://github.com/SamJamWillingham/react-weather-app">
+        <a
+          href="https://github.com/SamJamWillingham/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Open-Sourced Code
         </a>{" "}
         written by{" "}
-        <a href="https://www.linkedin.com/in/samantha-willingham-79576a1a8/">
+        <a
+          href="https://www.linkedin.com/in/samantha-willingham-79576a1a8/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Sam Willingham
         </a>
       </small>
       <br />
       <small>
         Icons made by{" "}
-        <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+        <a
+          href="https://www.flaticon.com/authors/freepik"
+          title="Freepik"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Freepik
         </a>{" "}
         from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
+        <a
+          href="https://www.flaticon.com/"
+          title="Flaticon"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           {" "}
           www.flaticon.com
         </a>
