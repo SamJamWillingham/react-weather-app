c47df9b7d8ed8c618c59c62a09f9607936a81527
Sam Willingham <68390031+SamJamWillingham@users.noreply.github.com>
2020-09-15 16:46:45 -0400

    added links to GitHub Repo and LinkedIn profile to footer

--- src/Credit.js
+++ src/Credit.js
@@ -5,6 +5,16 @@ export default function Credit() {
   return (
     <div id="credit">
       <small>
+        <a href="https://github.com/SamJamWillingham/react-weather-app">
+          Open-Sourced Code
+        </a>{" "}
+        written by{" "}
+        <a href="https://www.linkedin.com/in/samantha-willingham-79576a1a8/">
+          Sam Willingham
+        </a>
+      </small>
+      <br />
+      <small>
         Icons made by{" "}
         <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
           Freepik
